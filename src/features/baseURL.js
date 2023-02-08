185dde2b745d8132a24eea55ce66698ba0ee0350
Hieu Phan Trung <102812781+phantrunghieu91@users.noreply.github.com>
2023-02-09 00:41:03 +0700

    Update baseURL.js
    fixed gramma

--- src/features/baseURL.js
+++ src/features/baseURL.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // const baseURL = 'http://localhost:3001';
 
-const baseURL = 'http://managemoney-database.glitch.me';
+const baseURL = 'https://managemoney-database.glitch.me';
 
 const client = axios.create({ baseURL });
 
